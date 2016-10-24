a27a74e146f69cfe85997e1749030fd99f8aad42
lucas <luca.pisani@abstract.it>
2016-10-24 17:36:30 +0200

    added bulk view to create cover images

--- zen/extractpdfimage/views.py
+++ zen/extractpdfimage/views.py
@@ -1,14 +1,29 @@
+import transaction
+import logging
 from OFS.Image import File
 from zope.component import getUtility
 from Products.statusmessages.interfaces import IStatusMessage
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from plone.registry.interfaces import IRegistry
 from eea.converter.convert import Convert
+logger = logging.getLogger('zen.extractpdfimage')
 
 
 class ExtractCoverImage(BrowserView):
     def __call__(self):
-        obj = self.context
+        result = self.create_coverimage()
+        dst_url = self.context.absolute_url() + '/view'
+        if result:
+            messages = IStatusMessage(self.request)
+            messages.add(
+                u"The cover image has been saved into the image field",
+                type=u"success")
+            return self.request.response.redirect(dst_url)
+
+    def create_coverimage(self, obj=None):
+        if not obj:
+            obj = self.context
         registry = getUtility(IRegistry)
         dst_field_name = registry['zen.extractpdfimage.destfield']
         ffield = obj.getField('file')
@@ -24,9 +39,19 @@ class ExtractCoverImage(BrowserView):
         })
         image = File('ignored-id', 'ignored-title', imagedata, 'image/png')
         obj.getField(dst_field_name).set(obj, image)
-        dst_url = obj.absolute_url() + '/view'
-        messages = IStatusMessage(self.request)
-        messages.add(
-            u"The cover image has been saved into the image field",
-            type=u"success")
-        return self.request.response.redirect(dst_url)
+        return True
+
+
+class BulkCoverImage(ExtractCoverImage):
+
+    def __call__(self):
+        catalog = getToolByName(self.context, name="portal_catalog")
+        brains = catalog(
+            object_provides="Products.ATContentTypes.interfaces.file.IATFile")
+        for b in brains:
+            obj = b.getObject()
+            self.create_coverimage(obj)
+            transaction.commit()
+            logger.info('Analized %s - %s' % (obj.Tilte(), obj.absolute_url()))
+
+        return 'OK!'
